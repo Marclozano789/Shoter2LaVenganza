2244416c9fbbdcdbc1263272abfc556addea5062
MarcLozano777 <marc.lozano.escribano@enti.cat>
2025-12-07 12:43:03 +0100

    movimiento listo

--- Assets/scripts/Player/PlayerMovement.cs
+++ Assets/scripts/Player/PlayerMovement.cs
@@ -7,15 +7,40 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 10f;
     private float gravity = -9.81f;
 
+    public Transform groundCheck;
+    public float sphereRadius = 0.3f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
     Vector3 velocity;
-    
+
+
+    public float jumpHeight = 3;
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, sphereRadius, groundMask);
+        if (isGrounded && velocity.y < 0) 
+        {
+            velocity.y = -2f;
+        
+        }
+
        
         float x= Input.GetAxis( "Horizontal" );
         float z = Input.GetAxis("Vertical");
 
         Vector3 move= transform.right * x + transform.forward * z;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded )
+        {
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        
+        }
+
 
         characterController.Move( move * speed * Time.deltaTime );
         
