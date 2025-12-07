b6d78f9f1cc509a44c25a8d42afa6272c26eb92c
MarcLozano777 <marc.lozano.escribano@enti.cat>
2025-12-07 13:51:21 +0100

    My Angel Put the Devil in Me

---
+++ Assets/scripts/Weapon/Shot.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+
+    public Transform spwnPoint;
+
+    public GameObject bullet;
+
+    public float shotForce = 1500f;
+    public float shotRate = 0.5f;
+
+    private float shotrateTime = 0; 
+
+
+    
+    void Update()
+    {
+        
+    }
+}
